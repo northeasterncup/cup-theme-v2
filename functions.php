a3bcae7c308553a556493b54f725bfab6058f166
beenbiishop <ben@benbishopsb.com>
2022-09-22 17:11:39 -0400

    who knows

--- functions.php
+++ functions.php
@@ -100,7 +100,7 @@ function hello_world_function()
 {
   $request = engage_request('/organizations/organization');
   $stuff = '<h1 class="h1">Hello world! Today is a great day!</h1>';
-  $stuff2 = $request['totalItems'];
+  $stuff .= $request['totalItems'];
   return $stuff;
 }
 add_shortcode('hello_world', 'hello_world_function');
