3813c3a7e542f0dee80ef885d8ad6d32d47b07d0
beenbiishop <ben@benbishopsb.com>
2022-09-22 16:37:50 -0400

    add shortcode

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
 // style and scripts
+
+use function PHPSTORM_META\map;
+
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
 function bootscore_child_enqueue_styles()
 {
@@ -33,18 +36,21 @@ define('UTC_TIMESTAMP', date_format(UTC_TIME, "c"));
 class Engage_Api extends WP_HTTP
 {
   const BASE_URL = 'https://engage-api.campuslabs.com/api/v3.0';
-  public $apiKey = 'esk_live_f98d79b42f2b22e3a9f9aacdcc4bf758';
-  public $pageSize = '50';
+  const API_KEY = 'esk_live_f98d79b42f2b22e3a9f9aacdcc4bf758';
+  const PAGE_SIZE = '50';
   public $endpoint = '/organizations/member';
+  public array $args;
   public $method = 'GET';
   public $body = '';
-  public array $args;
   public array $headers;
 
-  public function engage_request($endpoint = NULL, $method = NULL, $body = NULL, $args = NULL, $headers = NULL)
+  public function engage_request($endpoint = NULL, $args = NULL, $method = NULL, $body = NULL, $headers = NULL)
   {
     if (is_null($endpoint)) {
       $endpoint = $this->endpoint;
+    }
+    if (is_null($args)) {
+      $args = $this->args;
     }
     if (is_null($method)) {
       $method = $this->method;
@@ -52,20 +58,17 @@ class Engage_Api extends WP_HTTP
     if (is_null($body)) {
       $body = $this->body;
     }
-    if (is_null($args)) {
-      $args = $this->args;
-    }
     if (is_null($headers)) {
       $headers = $this->headers;
     }
     $allArgs = array_merge(array(
-      'take' => $this->pageSize,
+      'take' => self::PAGE_SIZE,
       'skip' => '0'
     ), $args);
     $allHeaders = array_merge(
       array(
         'Accept' => 'application/json',
-        'X-Engage-Api-Key' => $this->apiKey
+        'X-Engage-Api-Key' => self::API_KEY
       ),
       $headers
     );
@@ -83,10 +86,13 @@ class Engage_Api extends WP_HTTP
     return $request;
   }
 
-  public function concat_pages($endpoint = NULL, $method = NULL, $body = NULL, $args = NULL, $headers = NULL)
+  public function concat_pages($endpoint = NULL, $args = NULL, $method = NULL, $body = NULL, $headers = NULL)
   {
     if (is_null($endpoint)) {
       $endpoint = $this->endpoint;
+    }
+    if (is_null($args)) {
+      $args = $this->args;
     }
     if (is_null($method)) {
       $method = $this->method;
@@ -94,14 +100,11 @@ class Engage_Api extends WP_HTTP
     if (is_null($body)) {
       $body = $this->body;
     }
-    if (is_null($args)) {
-      $args = $this->args;
-    }
     if (is_null($headers)) {
       $headers = $this->headers;
     }
     $objects = [];
-    $firstRequest = $this->engage_request($endpoint, $method, $body, $args, $headers);
+    $firstRequest = $this->engage_request($endpoint, $args, $method, $body, $headers);
     $firstRequestResponse = $firstRequest['response'];
     $decodedFirstRequest = json_decode($firstRequestResponse);
     $firstRequestItems = $decodedFirstRequest['items'];
@@ -113,9 +116,9 @@ class Engage_Api extends WP_HTTP
       $skip = 50;
       $remaining = TRUE;
       while ($remaining) {
-        $request = $this->engage_request($endpoint, $method, $body, array_merge($args, array(
+        $request = $this->engage_request($endpoint, array_merge($args, array(
           'skip' => $skip
-        )), $headers);
+        )), $method, $body, $headers);
         $response = $request['response'];
         $decoded = json_decode($response, true);
         $items = $decoded['items'];
@@ -130,3 +133,16 @@ class Engage_Api extends WP_HTTP
     return $objects;
   }
 }
+
+// Test events shortcode
+function engage_events_function()
+{
+  $object = new Engage_Api('/events/event', array(
+    'organizationIds' => CUP_ORGANIZATION_ID,
+    'excludeCoHosts' => 'false',
+    'includeSubmissionIds' => true
+  ), 'GET');
+  $request = $object->engage_request();
+  return $request['response'];
+}
+add_shortcode('engage_events', 'engage_events_function');
