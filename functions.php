8c535a9fa1abee7c8f70f34f50f51f1a0239c1ae
beenbiishop <ben@benbishopsb.com>
2022-09-26 12:49:36 -0400

    cup home events

--- functions.php
+++ functions.php
@@ -153,3 +153,102 @@ function cup_events_function()
   return $card;
 }
 add_shortcode('cup_events', 'cup_events_function');
+
+// Homepage Events
+function home_events_function()
+{
+  // Make the request for upcoming events
+  $request = engage_request('/events/event/', array(
+    'organizationIds' => CUP_ORGANIZATION_ID,
+    'endsAfter' => utcTimestamp(),
+    'excludeCoHosts' => 'false',
+    'includeSubmissionIds' => 'true'
+  ));
+
+  // Seperate the returned item subarray from the total items value
+  $items = $request['items'];
+  $totalItems = intval($request['totalItems']);
+
+  // Sort the returned events by start date/time
+  usort($items, function ($a, $b) {
+    if ($a['startsOn'] == $b['startsOn']) return 0;
+    return strtotime($a['startsOn']) - strtotime($b['startsOn']);
+  });
+
+  // Keep only the first three events in the array
+  if ($totalItems > 3) {
+    $items = array_slice($items, 0, 3);
+    $totalItems = 3;
+  }
+
+  // Show an alert if no upcoming events are scheduled
+  if ($totalItems <= 0) {
+    $html = '<div class="events home-events">';
+    $html .= '<div class="alert alert-info no-events">';
+    $html .= 'There are no scheduled upcoming events just yet. Stay tuned!';
+    $html .= '</div></div>';
+  }
+
+
+  if ($totalItems > 0) {
+    // Start HTML Row
+    $html = '<div class="events home-events row g-3">';
+
+    // HTML for each item (column)
+    foreach ($items as $item) {
+      $html .= '<div class="event-wrapper col-12 col-md-6 col-lg-4">';
+      $html .= '<div class="card border-dark event-card">';
+
+      // event image if there is one
+      if (strlen($item['imageUrl']) > 0) {
+        $html .= '<a href="https://neu.campuslabs.com/engage/event/' . $item['id'] . '" target="_blank">';
+        $html .= '<div role="img" aria-label="Image Uploaded for Event Cover Photo" alt ="Image Uploaded for Event Cover Photo" class="card-img-top event-img border-bottom border-dark" style="background-image: url(\'' . $item['imageUrl'] . '?preset=large-w\');">';
+        $html .= '</div></a>';
+      }
+
+      // event body
+      $html .= '<div class="card-body event-body">';
+
+      // event title
+      $html .= '<a href="https://neu.campuslabs.com/engage/event/' . $item['id'] . '" target="_blank">';
+      $html .= '<h3 class="card-title event-title">' . $item['name'];
+      $html .= '</h3></a>';
+
+      // event text
+      $html .= '<span class="card-text event-text">';
+
+      // event date
+      $html .= '<div class="event-date pb-1">';
+      $html .= '<div class="bi bi-pe-1 bi-calendar2-heart">' . $item['startsOn'];
+      $html .= '</span></div>';
+
+      // event location
+      $html .= '<div class="event-location">';
+      $html .= '<div class="bi bi-pe-1 bi-geo-alt-fill">' . $item['address']['name'];
+      $html .= '</span></div>';
+
+      // event button
+      $html .= '<a href="https://neu.campuslabs.com/engage/event/' . $item['id'] . '" class="btn btn-primary bi bi-pe-2 bi-box-arrow-up-right mt-3" target="_blank">';
+      $html .= 'Learn More';
+      $html .= '</a>';
+
+      // closing tags
+      $html .= '</div></div></div>';
+
+      // add additional column if only one or two events
+      if ($totalItems == 1 || $totalItems == 2) {
+        $html .= '<div class="event-wrapper col-12 col-md-6 col-lg-4"></div>';
+      }
+
+      // add additional column if only one event
+      if ($totalItems == 1) {
+        $html .= '<div class="event-wrapper col-12 col-md-6 col-lg-4"></div>';
+      }
+      $html .= '</div>';
+    }
+
+    // return the html
+    return $html;
+  }
+}
+add_shortcode('home_events', 'home_events_function');
