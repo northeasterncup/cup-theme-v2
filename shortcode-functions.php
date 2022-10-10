8a6d0a9cc98e896a5171f3bf89f658b335991b39
beenbiishop <ben@benbishopsb.com>
2022-10-10 17:19:25 -0400

    fully disable caching temp

--- shortcode-functions.php
+++ shortcode-functions.php
@@ -205,7 +205,7 @@ add_shortcode('upcoming_events', 'upcoming_events_function');
 function past_events_function()
 {
     // Make the request for upcoming events
-    $request = engage_request_cached('past_events', 1, '/events/event/', array(
+    $request = engage_request('/events/event/', array(
         'organizationIds' => CUP_ORGANIZATION_ID,
         'endsBefore' => utcTimestamp(),
         'excludeCoHosts' => 'false',
