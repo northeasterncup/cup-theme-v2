d25dbac6fd5df15f81f38b64598dd7cbc077b653
beenbiishop <ben@benbishopsb.com>
2022-10-11 00:52:16 -0400

    add placeholder

--- event-options.php
+++ event-options.php
@@ -86,7 +86,7 @@ function engage_setting_base_url()
 {
     $setting = get_option('engage_base_url');
 ?>
-    <input type="url" name="engage_base_url" pattern="https://.*" size="40" value="<?php echo isset($setting) ? esc_attr($setting) : ''; ?>">
+    <input type="url" name="engage_base_url" pattern="https://.*" size="40" placeholder="https://engage-api.campuslabs.com/api/v3.0" value="<?php echo isset($setting) ? esc_attr($setting) : ''; ?>">
     <p class="description">
         The endpoint used for all API requests. This should not change unless a new API version is released.
     </p>
