7790ad3a5f16422ff3e7f4d8785f548a68cc0847
beenbiishop <ben@benbishopsb.com>
2022-10-10 22:35:33 -0400

    Initial options page

--- event-options.php
+++ event-options.php
@@ -18,5 +18,22 @@ function engage_settings_page()
     }
     echo '<div class="wrap">';
     echo '<h1>CampusLabs Engage Settings</h1>';
+    echo '<p>Events are displayed throughout the site from the CampusLabs Engage Platform, using their ';
+    echo 'REST API. New events will be automatically pulled every 5 minutes. Configure the integration ';
+    echo 'settings below if needed, but these should not need to change often.</p>';
+    echo '<h2 class="title">Integration Settings</h2>';
+    echo '<h2 class="title">Available Shortcodes</h2>';
+    echo '<p>Currently, three shortcodes are available:';
+    echo '<ol>';
+    echo '<li><code>[homepage_events]</code> - Displays the next three upcoming events</li>';
+    echo '<li><code>[upcoming_events]</code> - Displays all upcoming events</li>';
+    echo '<li><code>[past_events]</code> - Displays past events that take place after the specified cutoff date</li>';
+    echo '<h2 class="title">Creating an Event</h2>';
+    echo '<p>To add new events, you must have an assigned position on CUP\'s roster. New events can be created ';
+    echo 'by going to <a href="https://neu.campuslabs.com/engage/">Engage</a>, clicking "CUP" under ';
+    echo '"My Organizations", and clicking "Events" under "Organization Tools". Once you go through the ';
+    echo 'event creation process, an email will be sent to the CUP advisor to confirm the event details are ';
+    echo 'valid, and once approved, the event will automatically posted on the CUP website, the Student Hub, ';
+    echo 'and other various Northeastern platforms.</p>';
     echo '</div>';
 }
