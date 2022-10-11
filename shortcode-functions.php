52a4e1306a5bb00ec7e9204be0c7c7755e360d80
beenbiishop <ben@benbishopsb.com>
2022-10-10 21:47:34 -0400

    cache all good

--- shortcode-functions.php
+++ shortcode-functions.php
@@ -208,6 +208,7 @@ function past_events_function()
     $request = engage_request_concat_cached('past_events', 300, '/events/event/', array(
         'organizationIds' => CUP_ORGANIZATION_ID,
         'endsBefore' => utcTimestamp(),
+        'startsAfter' => '2022-09-01',
         'excludeCoHosts' => 'false',
         'includeSubmissionIds' => 'true'
     ));
